688e457f5c1aad9924a72399aaa3cc0d9be9ac34
lamdan0901 <itmaster912@gmail.com>
2022-06-03 08:58:36 +0700

    config for vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
+import svgrPlugin from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3006,
+  },
+  preview: {
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    // reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+})
